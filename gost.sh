934fa3a983bd7ddfdf9a217f62cfbfb10e9bb458
Sam <sxx20001227@gmail.com>
2021-09-17 21:35:17 +0800

    Update gost.sh

--- gost.sh
+++ gost.sh
@@ -109,8 +109,8 @@ function Install_ct() {
     mv gost-linux-"$bit"-"$ct_new_ver" gost
     mv gost /usr/bin/gost
     chmod -R 777 /usr/bin/gost
-    wget --no-check-certificate https://cdn.jsdelivr.net/gh/KANIKIG/Multi-EasyGost@master/gost.service && chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system
-    mkdir /etc/gost && wget --no-check-certificate https://cdn.jsdelivr.net/gh/KANIKIG/Multi-EasyGost@master/config.json && mv config.json /etc/gost && chmod -R 777 /etc/gost
+    wget --no-check-certificate https://cdn.jsdelivr.net/gh/sam-sun00/Multi-EasyGost@master/gost.service && chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system
+    mkdir /etc/gost && wget --no-check-certificate https://cdn.jsdelivr.net/gh/sam-sun00/Multi-EasyGost@master/config.json && mv config.json /etc/gost && chmod -R 777 /etc/gost
   fi
 
   systemctl enable gost && systemctl restart gost
@@ -851,14 +851,14 @@ cron_restart() {
 }
 
 update_sh() {
-  ol_version=$(curl -L -s --connect-timeout 5 https://cdn.jsdelivr.net/gh/KANIKIG/Multi-EasyGost@master/gost.sh | grep "shell_version=" | head -1 | awk -F '=|"' '{print $3}')
+  ol_version=$(curl -L -s --connect-timeout 5 https://cdn.jsdelivr.net/gh/sam-sun00/Multi-EasyGost@master/gost.sh | grep "shell_version=" | head -1 | awk -F '=|"' '{print $3}')
   if [ -n "$ol_version" ]; then
     if [[ "$shell_version" != "$ol_version" ]]; then
       echo -e "存在新版本，是否更新 [Y/N]?"
       read -r update_confirm
       case $update_confirm in
       [yY][eE][sS] | [yY])
-        wget -N --no-check-certificate https://cdn.jsdelivr.net/gh/KANIKIG/Multi-EasyGost@master/gost.sh
+        wget -N --no-check-certificate https://cdn.jsdelivr.net/gh/sam-sun00/Multi-EasyGost@master/gost.sh
         echo -e "更新完成"
         exit 0
         ;;
